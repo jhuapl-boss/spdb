c7905e6a954e548d98f8a8b4d4ac8e1209a5c67e
Kleissas, Dean M <dean.kleissas@jhuapl.edu>
2016-03-29 15:05:52 -0400

    Removed mocking for bossutils logger
    The mocking wasn't working due to how the logger is getting configuring the log file.  Need to change the implementation if we want to be able to mock this.  Right now unit tests that use the logger will fail locally.

--- spatialdb/test/test_error.py
+++ spatialdb/test/test_error.py
@@ -1,25 +1,10 @@
 import unittest
 from spatialdb.error import SpdbError
-import bossutils
-import tempfile
-import os
-from bossutils.logger import BossLogger
-from unittest.mock import patch
 
 
-class MockBossLogger(BossLogger):
-    """Basic mock for BossConfig to contain the properties needed for this test"""
-    LOG_FILE = os.path.join(tempfile.TemporaryDirectory().name, 'test_logfile.log')
-
-    def __init__(self):
-        BossLogger.__init__(self)
-        self.LOG_FILE = os.path.join(tempfile.TemporaryDirectory().name, 'test_logfile.log')
-
-
-@patch('bossutils.logger.BossLogger', MockBossLogger)
 class SpdbErrorTests(unittest.TestCase):
 
-    def test_creation(self):
+    def test_creation(self, mock_resource_filename):
         with self.assertRaises(SpdbError):
             raise SpdbError('whoops', 'Something went wrong!', 2000)
 
