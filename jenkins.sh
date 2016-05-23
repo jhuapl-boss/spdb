4d49af93b8a3dca13d695c68f3a5d7cc852fa972
Tim Gion <timothy.gion@jhuapl.edu>
2016-05-23 16:36:06 -0400

    Use absolute path to C library.

--- jenkins.sh
+++ jenkins.sh
@@ -2,12 +2,13 @@
 
 # This script is run by Jenkins.
 
+# $WORKSPACE is set by Jenkins.
+cd $WORKSPACE/spdb/c_lib/c_version
+
 # Ensure spdb C code built.
-cd spdb/c_lib/c_version
 cp makefile_LINUX makefile
 make all
 
-# $WORKSPACE is set by Jenkins.
 cd $WORKSPACE
 
 # Set path so the old spdb in python3/site-packages not used.
