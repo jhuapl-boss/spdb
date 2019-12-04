7a6534c601877ef51d49e9d8ef39099ca14574d1
Derek Pryor <Derek.Pryor@jhuapl.edu>
2019-12-03 22:19:17 -0800

    Clean up of old files

--- nose2.sh
+++
@@ -1,8 +0,0 @@
-# Make sure tests will use mocked environment, in case credentials are available
-# via another mechanism
-export AWS_ACCESS_KEY_ID="testing"
-export AWS_SECRET_ACCESS_KEY="testing"
-export AWS_SECURITY_TOKEN="testing"
-export AWS_SESSION_TOKEN="testing"
-
-exec nose2 $*
